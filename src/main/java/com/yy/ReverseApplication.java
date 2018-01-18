d49d4a0a8c4e9094860a8af7985e86dec5eadcf5
yygnai <595703420@qq.com>
2018-01-18 19:06:46 +0800

    init
    123456

---
+++ src/main/java/com/yy/ReverseApplication.java
@@ -0,0 +1,12 @@
+package com.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseApplication.class, args);
+	}
+}
